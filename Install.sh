7ccc4fc17483ff89c0326e4350046116c956c7c7
cfrankovich <carsonfrankovich6@gmail.com>
2021-08-13 00:55:40 -0400

    Updating install script

--- Install.sh
+++ Install.sh
@@ -12,6 +12,7 @@ neg="[${red}${bold}-${reset}]"
 neu="[${blue}${bold}o${reset}]"
 
 SRCDIR=$(pwd)
+ver="1.0"
 
 echo -e "\n${red}>${blue}>${green}> ${reset}Installing ${bold}Styre${reset} from ${bold}https://github.com/cfrankovich/styre${reset} ${green}<${blue}<${red}<${reset}\n"
 
@@ -32,7 +33,7 @@ echo -e "${neu} Creating config file."
 if [ ! -e config ]
 then
 	touch config 
-	echo -e "DIR \"${STYREDIR}\"\nCGOAL \"\"\nNUMLIST \"0\"\nDEFCOM \"-G\"\n" > config 
+	echo -e "DIR \"${STYREDIR}\"\nCGOAL \"\"\nNUMLIST \"0\"\nDEFCOM \"-g\"\n" > config 
 	echo -e "${pos} Config file created!"
 else
 	echo -e "${neg} Config file already found! Skipping process..."
@@ -57,9 +58,27 @@ else
 fi
 
 # Compile #
+echo -e "${neu} Compiling source code."
+cd $SRCDIR 
+make >/dev/null 2>&1
+if [ -e styre ]
+then
+	echo -e "${pos} Code compiled successfully!"
+else
+	echo -e "${neg} Code failed to compile!"
+	exit 1
+fi
 
 # Move to bin #
+echo -e "${neu} Moving binary"
+sudo mv styre /usr/bin/styre
+if [ -e /usr/bin/styre ]
+then
+	echo -e "${pos} Move complete!"
+else
+	echo -e "${neg} Could not move bianry"
+fi
 
 # Finish #
-echo -e "${pos} Installation complete! Please run ${bold}styre -h${reset} for help.\n" 
+echo -e "${pos} Styre version ${ver} has been installed!\n" 
 
